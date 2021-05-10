9adc65e9fb88dbcc024887777487cc51afc6288b
Akilesh <akilesh04.ss@gmail.com>
2021-05-10 21:52:54 +0530

    p

---
+++ biobin_/src/components/function/header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <body>
+        <div>include ="head.html"</div>
+         <script>
+             includeHTML();
+         </script>
+         </body>
+    )
+}
+
+export default Header
