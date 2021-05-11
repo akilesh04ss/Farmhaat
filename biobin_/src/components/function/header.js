e11a3ebb1e0de7fe29ad790203a281f3e5514f71
Akilesh <akilesh04.ss@gmail.com>
2021-05-11 10:16:42 +0530

    html

--- biobin_/src/components/function/header.js
+++ biobin_/src/components/function/header.js
@@ -1,14 +1,12 @@
 import React from 'react'
+import logo from '<img src="https://www.shutterstock.com/image-vector/flat-menu-icon-illustration-website-navigation-633946220" alt="" class="">'
 
 function Header() {
     return (
-        <body>
-        <div>include ="head.html"</div>
-         <script>
-             includeHTML();
-         </script>
-         </body>
+       <body>
+      <img src = {logo} alt ="Logo"/>;
+       </body> 
     )
 }
 
-export default Header
+export default Header;
